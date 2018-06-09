144bdd180400147e0370531cdd996ec860cade32
wulinman <550832921@qq.com>
2018-06-09 16:29:37 +0800

    add camera focus script
    新增AR相机对焦脚本

---
+++ Assets/Script/CameraFocus.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFocus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vuforia.CameraDevice.Instance.SetFocusMode(Vuforia.CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
