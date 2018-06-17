e9dbdba3d69daf19fa0fda06c02937a0746e9043
Cocaice <953447162@qq.com>
2018-06-17 23:45:08 +0800

    添加通关界面

---
+++ Assets/Script/AllKill.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllKill : MonoBehaviour {
+    public GameObject WinCanvas;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (this.transform.childCount == 3)
+        {
+            WinCanvas.SetActive(true);
+        } else
+        {
+            //WinCanvas.SetActive(false);
+        }
+        
+    }
+}
