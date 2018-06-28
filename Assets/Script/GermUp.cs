e28e8d4eef06d91520d2f3f5cea48a5920a7abd7
wulinman <550832921@qq.com>
2018-06-28 15:56:49 +0800

    finshed game scene1
    完成关卡1的细菌布置

---
+++ Assets/Script/GermUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GermUp : MonoBehaviour {
+	public float showTime; // 出场时间
+	public float lastTime; // 持续时间
+	//public GameObject germ;
+	// Use this for initialization
+	void Start () {
+		//showTime= 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.timeSinceLevelLoad > showTime && Time.timeSinceLevelLoad < showTime+lastTime) {
+			transform.Translate (0, Time.deltaTime * 5, 0);
+		}
+	}
+}
