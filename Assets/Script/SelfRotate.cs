693cfe9f76545c8205d6817e6079dceb58bf76c0
wulinman <550832921@qq.com>
2018-06-29 02:43:17 +0800

    GermBattle Final Version 1.0
    1. 完成所有场景的布置和细菌摆放
    2. 完成规则页面设计

---
+++ Assets/Script/SelfRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfRotate : MonoBehaviour
+{
+    public GameObject germ;
+    public float X;
+    public float Y;
+    public float Z;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        germ.transform.Rotate(X, Y, Z);
+    }
+}
